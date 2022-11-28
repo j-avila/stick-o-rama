93b102018822081d69a3b167e21f7fb4b58e39e4
j-avila <jose.leon.avila@gmail.com>
2022-11-27 22:21:36 -0300

    feature: default state, search and results

--- scripts.js
+++ scripts.js
@@ -6,23 +6,23 @@ const env = {
 class List extends HTMLElement {
   constructor() {
     super()
-    let shadow = this.attachShadow({ mode: "open" })
-    this.divheader = document.createElement("div")
-    this.divheader.innerHTML = "hola mundo"
-    shadow.appendChild(this.divheader)
-    this.state = undefined
+    this.shadow = this.attachShadow({ mode: "open" })
+    this.divheader = document.createElement("h1")
+    this.searchParam = document.getElementById("search-param")
+    this.state = {}
     this.render()
   }
 
   getData = async (params) => {
     const trend = `${env.apiURL}/trending?api_key=${env.apikey}&limit=25&rating=g`
-    const search = `${env.apiURL}/search?api_key=${env.apikey}&q=${params}&limit=25&offset=0&rating=g&lang=es?`
+    const search = `${env.apiURL}/search?api_key=${env.apikey}&q=${params}&limit=5&offset=0&rating=g&lang=es?`
     fetch(params ? search : trend, {
       headers: { "Content-Type": "application/json" },
     })
       .then((resp) => resp.json())
       .then((resp) => {
-        return resp.data
+        this.state = { data: resp.data }
+        this.render()
       })
       .catch((err) => {
         console.log(err)
@@ -33,20 +33,60 @@ class List extends HTMLElement {
   connectedCallback() {
     const title = this.getAttribute("title")
     this.divheader.innerHTML = `hola ${title}`
-    this.state = this.getData()
+    this.shadow.prepend(this.divheader)
+    this.getData()
+    this.handlechange()
+  }
+
+  //TODO no tener que hacer focus off para disparar la acción
+  handlechange = () => {
+    this.searchParam.addEventListener("change", ({ target }) => {
+      console.log(target.value)
+      this.getData(target.value)
+    })
+  }
+
+  //TODO descarga de sticker
+  async downloadImage(imageSrc) {
+    const urlImg = `https://i.giphy.com/media/${imageSrc?.images?.original?.hash}/giphy.webp`
+    // const image = await fetch(imageSrc?.images?.original.url)
+    const image = await fetch(urlImg)
+    const imageBlog = await image.blob()
+    const imageURL = URL.createObjectURL(imageBlog)
+
+    const link = document.createElement("a")
+    link.href = imageURL
+    link.download = imageSrc.title
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
   }
 
   render() {
-    if (this.state) {
-      console.log(this.state)
-      this.state.map((item) => {
+    console.log(this.state)
+    this.shadow.replaceChildren()
+    if (this.state?.data?.length >= 1) {
+      const { data } = this.state
+      let results = document.createElement("ul")
+      results.style.display = "grid"
+      results.style.margin = "0 auto"
+      results.style.paddingLeft = "0"
+      results.style.width = "100%"
+      results.style.listStyle = "none"
+      results.style.gap = "10px"
+      results.style.gridTemplateColumns =
+        "repeat(auto-fill, minmax(100px, 1fr))"
+      results.id = "resultsList"
+      results.classList.add("resultsList")
+      this.shadow.appendChild(results)
+      data.map((item) => {
         let listItem = document.createElement("li")
-        listItem.innerHTML = `<img src="${item.images.original.url}" /></li>`
-        this.shadow.appendChild(listItem)
+        listItem.innerHTML = `<img style='width: 100%; cursor: pointer' src="${item.images.original.url}" />`
+        listItem.addEventListener("click", () => this.downloadImage(listItem))
+        results.appendChild(listItem)
       })
     } else {
-      console.log(this.shadow)
-      this.shadow.innerHTML = `<h1>nada que ver</h1>`
+      this.shadow.innerHTML = `<h3>nada que ver</h3>`
     }
   }
 }
